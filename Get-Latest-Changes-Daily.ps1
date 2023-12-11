404f7bb801c5eb3206fea369b6bb5ec134dfff2e
Anthony Frank <afrank84@yahoo.com>
2023-12-11 11:24:58 -0500

    Update Get-Latest-Changes-Daily.ps1
    Added in logic for checking sha before download.

--- Get-Latest-Changes-Daily.ps1
+++ Get-Latest-Changes-Daily.ps1
@@ -1,1 +1,36 @@
-Invoke-WebRequest -Uri https://github.com/afrank84/dr-greenwalt-automation/archive/refs/heads/main.zip -OutFile C:\stash\dr-greenwalt-automation-main.zip
+#Invoke-WebRequest -Uri https://github.com/afrank84/dr-greenwalt-automation/archive/refs/heads/main.zip -OutFile C:\stash\dr-greenwalt-automation-main.zip
+
+$owner = "username"
+$repo = "repository"
+$branch = "main"
+$localFile = "path\to\local\commitSHA.txt"
+
+# Function to get the latest commit SHA from GitHub
+function Get-LatestCommitSHA {
+    $uri = "https://api.github.com/repos/$owner/$repo/commits/$branch"
+    $response = Invoke-RestMethod -Uri $uri -Headers @{ "User-Agent" = "PowerShell" }
+    return $response.sha
+}
+
+# Read the last known commit SHA
+$lastKnownSHA = ""
+if (Test-Path $localFile) {
+    $lastKnownSHA = Get-Content $localFile
+}
+
+# Get the latest commit SHA from GitHub
+$latestSHA = Get-LatestCommitSHA
+
+# Compare and download if different
+if ($latestSHA -ne $lastKnownSHA) {
+    $downloadUrl = "https://github.com/$owner/$repo/archive/$branch.zip"
+    $outputFile = "$repo-$branch.zip"
+    Invoke-WebRequest -Uri $downloadUrl -OutFile $outputFile
+    # Optionally, extract the ZIP file as shown in previous answer
+    # Update the local commit SHA file
+    $latestSHA | Set-Content $localFile
+    Write-Host "Repository updated and downloaded."
+} else {
+    Write-Host "Repository is up-to-date. No download required."
+}
+
