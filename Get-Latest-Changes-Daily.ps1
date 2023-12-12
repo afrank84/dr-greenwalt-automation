781f2da078dea8b00074420d2d5f05420f67d435
Anthony Frank <afrank84@yahoo.com>
2023-12-12 11:56:57 -0500

    Get-Latest-Changes-Daily.ps1 - Downloads only script now.

--- Get-Latest-Changes-Daily.ps1
+++ Get-Latest-Changes-Daily.ps1
@@ -1,37 +1,8 @@
-#Invoke-WebRequest -Uri https://github.com/afrank84/dr-greenwalt-automation/archive/refs/heads/main.zip -OutFile C:\stash\dr-greenwalt-automation-main.zip
+# Define the URL of the file to download
+$url = "https://raw.githubusercontent.com/afrank84/dr-greenwalt-automation/main/Automate-Desktop.ps1"
 
-# Define the URL of the GitHub ZIP file
-$githubZipUrl = 'https://github.com/afrank84/dr-greenwalt-automation/archive/refs/heads/main.zip'
+# Define the path where the file will be saved
+$destination = "C:\path\to\save\Automate-Desktop.ps1"  # Change the path as needed
 
-# Define the destination folder
-$destinationFolder = 'C:\stash\'
-
-# Define the path for the ZIP file
-$zipFilePath = Join-Path -Path $destinationFolder -ChildPath 'dr-greenwalt-automation-main.zip'
-
-# Define the extraction subfolder
-$extractionSubFolder = Join-Path -Path $destinationFolder -ChildPath 'dr-greenwalt-automation-main'
-
-# Check and create the destination folder if it does not exist
-if (-not (Test-Path -Path $destinationFolder)) {
-    New-Item -Path $destinationFolder -ItemType Directory
-}
-
-# Check if the extraction subfolder exists, and if so, delete it
-if (Test-Path -Path $extractionSubFolder) {
-    Remove-Item -Path $extractionSubFolder -Recurse -Force
-}
-
-# Download the ZIP file
-Invoke-WebRequest -Uri $githubZipUrl -OutFile $zipFilePath
-
-# Unzip the file
-Expand-Archive -Path $zipFilePath -DestinationPath $destinationFolder
-
-# Remove the ZIP file
-Remove-Item -Path $zipFilePath
-
-# Output message
-Write-Host "ZIP file downloaded and extracted to $destinationFolder"
-
-
+# Use Invoke-WebRequest to download the file
+Invoke-WebRequest -Uri $url -OutFile $destination
