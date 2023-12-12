0688e87a89275be1d2c590f014a6195a7283ceb2
Anthony Frank <afrank84@yahoo.com>
2023-12-12 12:09:40 -0500

    Get-Latest-Changes-Daily.ps1 - added main

--- Get-Latest-Changes-Daily.ps1
+++ Get-Latest-Changes-Daily.ps1
@@ -2,7 +2,7 @@
 $url = "https://raw.githubusercontent.com/afrank84/dr-greenwalt-automation/main/Automate-Desktop.ps1"
 
 # Define the path where the file will be saved
-$destination = "C:\stash\dr-greenwalt-automation\Automate-Desktop.ps1"  # Change the path as needed
+$destination = "C:\stash\dr-greenwalt-automation-main\Automate-Desktop.ps1"  # Change the path as needed
 
 # Use Invoke-WebRequest to download the file
 Invoke-WebRequest -Uri $url -OutFile $destination
